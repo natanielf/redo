143e488640fd94b0241fc32eb5b26f424cbcafda
natanielf <natanielfarzan@gmail.com>
2022-02-22 19:13:42 -0800

    Implement logic for adding tasks

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 slint::include_modules!();
 
 fn main() {
+    let tasks = Vec::new();
+
     let ui = AppWindow::new();
+
+    ui.on_add_task({
+        let mut tasks = tasks.clone();
+        move |text| {
+            tasks.push(Task {
+                text,
+                is_completed: false,
+            })
+        }
+    });
 
     ui.run();
 }
